7621b5d77befbffa9a3fb3c81c7a1bbdf4d4a5bb
Oruga <kanojodo@gmail.com>
2023-09-06 21:49:51 +0200

    first commit - html,css,initial js

---
+++ script.js
@@ -0,0 +1,17 @@
+const username = document.querySelector('#username')
+const pass = document.querySelector('#password')
+const pass2 = document.querySelector('#password2')
+const email = document.querySelector('#email')
+const clearBtn = document.querySelector('.clear')
+const sendBtn = document.querySelector('.send')
+const popup = document.querySelector('.popup')
+const closeBtn = document.querySelector('.close')
+
+clearBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    [username, pass, pass2, email].forEach(el => {
+        el.value = "";
+    });
+
+})
